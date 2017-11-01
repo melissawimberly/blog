fdf647cbc42f5b8f7b6efdafb0b9f8b43242c866
Melissa Wimberly <melissa.f.wimberly@gmail.com>
2017-11-01 13:06:40 -0400

    Make tab specific for rendering components -WIP

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -11,6 +11,17 @@ import Footer  from './Footer';
 
 class Home extends React.Component  {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      tab: 'recipe'
+    }
+  }
+
+  getPageBody = () => {
+    console.log('getting tab');
+  }
+
   render() {
     return(
       <div className="home-container-wrapper">
