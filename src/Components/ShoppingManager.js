a8c1e5b75581cd39478aa0301b43ba1ba0c54d46
Melissa Wimberly <melissa.f.wimberly@gmail.com>
2017-12-21 00:36:46 -0500

    Update the Shopping section to click to next image

--- src/Components/ShoppingManager.js
+++ src/Components/ShoppingManager.js
@@ -3,26 +3,48 @@ const shoppingCards = require('../data').shopping;
 
 class ShoppingManager extends React.Component {
 
-  getActiveCard = () => {
-    return shoppingCards.map((card, index) => {
-      return (
-        <div className="shopping-card" key={`${index}-shopping-card`}>
-          <div className="shopping-card-box">
-            <div className="box-title">{card.title}</div>
-            <div className="box-description">{card.desc}</div>
-            <button className="box-button">{card.button_text}</button>
-          </div>
-        </div>
-      )
-    })
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeCardIndex: 0
+    }
+  }
+
+  getNextCard = () => {
+    return(e) => {
+      let { activeCardIndex } = this.state;
+      this.setState({ activeCardIndex: activeCardIndex < 2 ? (activeCardIndex + 1) : 0 })
+    }
+  };
+
+  getPreviousCard = () => {
+    return(e) => {
+      let { activeCardIndex } = this.state;
+      this.setState({ activeCardIndex: (activeCardIndex - 1) })
+    }
   };
 
 
+
   render(){
+    let { activeCardIndex } = this.state;
+    const shoppingBoxStyle = {
+      background: `url(${shoppingCards[activeCardIndex].image_source})`,
+      height: '385px',
+      zIndex: '1'
+    };
     return (
       <div className="shopping-manager-wrapper">
-        SHOPPING SECTION
-        { this.getActiveCard() }
+        <div
+          className="shopping-card" key={`${activeCardIndex}-shopping-card`} onClick={this.getNextCard()} style={shoppingBoxStyle}>
+          <div className="carousel-arrow-forward"><span class="glyphicon glyphicon-chevron-right"></span></div>
+          <div className="shopping-card-box">
+            <h2 className="box-title">{shoppingCards[activeCardIndex].title}</h2>
+            <p className="box-description">{shoppingCards[activeCardIndex].desc}</p>
+            <button className="action-button">{shoppingCards[activeCardIndex].button_text}</button>
+          </div>
+          <div className="carousel-arrow-backward"><span class="glyphicon glyphicon-chevron-left"></span></div>
+        </div>
       </div>
     )
   }
