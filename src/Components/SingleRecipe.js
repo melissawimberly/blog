d003a20d78dc0643ff6befa1da0079e9602054dd
Melissa Wimberly <melissa.f.wimberly@gmail.com>
2017-12-21 00:37:36 -0500

    Add images to recipes with styling

--- src/Components/SingleRecipe.js
+++ src/Components/SingleRecipe.js
@@ -3,14 +3,14 @@ import React, { Component } from 'react';
 class SingleRecipe extends React.Component {
   render(){
     let { recipe } = this.props;
-    // let recipe = Object.values(this.props.recipe)[0];
-    let { title, short_desc, long_desc } = recipe;
+    let { title, short_desc, long_desc, image_source } = recipe;
 
     return (
-      <div className="recipe-wrapper" key={`${title}-recipe`}>
+      <div className="recipe-wrapper clickable" key={`${title}-recipe`}>
+        <div className="recipe-image-wrap">
+          <img className="recipe-image" src={image_source} />
+        </div>
         <div className="recipe-title">{title}</div>
-        <div className="recipe-short-desc">{short_desc}</div>
-        <div className="recipe-long-desc">{long_desc}</div>
       </div>
     )
   }
