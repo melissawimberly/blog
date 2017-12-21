f9024db3553239be69e6388c17ba30232f673393
Melissa Wimberly <melissa.f.wimberly@gmail.com>
2017-12-21 01:35:09 -0500

    Add classes and hover logic for recipes

--- src/Components/SingleRecipe.js
+++ src/Components/SingleRecipe.js
@@ -1,14 +1,26 @@
 import React, { Component } from 'react';
 
 class SingleRecipe extends React.Component {
+
+  showHoverDetails = () => {
+    return(e) => {
+
+    }
+  }
+
   render(){
     let { recipe } = this.props;
-    let { title, short_desc, long_desc, image_source } = recipe;
+    let { title, short_desc, long_desc, difficulty_level, total_time, prep_time, image_source } = recipe;
 
     return (
       <div className="recipe-wrapper clickable" key={`${title}-recipe`}>
-        <div className="recipe-image-wrap">
+        <div className="recipe-image-wrap" onMouseOver={this.showHoverDetails()}>
           <img className="recipe-image" src={image_source} />
+          <div className="hover-image-text-wrapper">
+            <p className="hover-image-textr">Prep: {prep_time}</p>
+            <p className="hover-image-textr">Total: {total_time}</p>
+            <p className="hover-image-textr">Difficulty: {difficulty_level}</p>
+          </div>
         </div>
         <div className="recipe-title">{title}</div>
       </div>
