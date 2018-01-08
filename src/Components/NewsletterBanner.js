0c1e53d71e0bb9e71e3f1c000b804bec16e6e99f
Melissa Wimberly <melissa.f.wimberly@gmail.com>
2018-01-08 00:06:08 -0500

    Fix Newsletter to the bottom -WIP

--- src/Components/NewsletterBanner.js
+++ src/Components/NewsletterBanner.js
@@ -5,7 +5,7 @@ class NewsletterBanner extends React.Component {
 
   render() {
     return (
-      <div className="books-wrapper">
+      <div className="newsletter-banner-wrapper">
         <h1>Sign up to our Newsletter</h1>
         <input placeholder="Enter your email"></input>
         <button>Send</button>
