95eada4cb9a0e1f1de7ef55252b43f01786b59c3
Melissa Wimberly <melissa.f.wimberly@gmail.com>
2018-01-08 00:05:33 -0500

    Update delis - sans backgrounds

--- src/Components/Delis.js
+++ src/Components/Delis.js
@@ -3,65 +3,27 @@ const locations = require('../data').locations;
 
 class Delis extends React.Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      activeCardIndex: 0,
-      carouselCardCount: 3
-    }
-  }
+  getLocations = () => {
+    // const backgroundStyle = {
+    //   background: `url(${shoppingCards[activeCardIndex].image_source})`,
+    // };
 
-  getNextCard = () => {
-    return(e) => {
-      let { activeCardIndex } = this.state;
-      this.setState({ activeCardIndex: activeCardIndex < 2 ? (activeCardIndex + 1) : 0 })
-    }
-  };
-
-  getPreviousCard = () => {
-    return(e) => {
-      let { activeCardIndex, carouselCardCount } = this.state;
-      this.setState({ activeCardIndex: !!activeCardIndex ? (activeCardIndex - 1) : (carouselCardCount - 1) })
-    }
-  };
-
-  // getLocations = () => {
-  //   return locations.map((location, index) => {
-  //     return (
-  //       <div className="location-wrapper" key={`${location}-${index}`}>
-  //         <div className="location-text-box">
-  //           <div className="location-title">{location.title}</div>
-  //         </div>
-  //       </div>
-  //     )
-  //   })
-  // }
-  render(){
-    let { activeCardIndex } = this.state;
-    const deliBoxStyle = {
-      // background: `url(${shoppingCards[activeCardIndex].image_source})`,
-      height: '385px',
-      zIndex: '1'
-    };
-    
-    return (
-      <div className="deli-locations-wrapper">
-        <div
-          className="deli-card"
-          key={`${activeCardIndex}-deli-card`}
-          style={deliBoxStyle}>
-          <div className="carousel-arrow-forward" onClick={this.getNextCard()}>
-            <span className="glyphicon glyphicon-chevron-right"></span>
-          </div>
-          <div className="deli-card-box">
-            <h2 className="box-title">{locations[activeCardIndex].title}</h2>
-            <p className="box-description">{locations[activeCardIndex].desc}</p>
-            <button className="action-button">{locations[activeCardIndex].button_text}</button>
-          </div>
-          <div className="carousel-arrow-backward" onClick={this.getPreviousCard()}>
-            <span className="glyphicon glyphicon-chevron-left"></span>
+    return locations.map((location, index) => {
+      return (
+        <div className="deli-card" key={`${location}-${index}`} /*style={backgroundStyle}*/>
+          <div className="deli-text-box">
+            <h1 className="deli-title">{location.title}</h1>
+            <h4 className="deli-see-more-text">See More</h4>
           </div>
         </div>
+      )
+    })
+  }
+
+  render(){
+    return (
+      <div className="deli-locations-wrapper">
+        { this.getLocations() }
       </div>
     )
   }
