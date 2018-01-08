737b87848c9d717b8285f35f28d20881809eb2c8
Melissa Wimberly <melissa.f.wimberly@gmail.com>
2018-01-08 15:22:10 -0500

    Update App with routes

--- src/App.js
+++ src/App.js
@@ -1,52 +1,159 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import logo from './logo.svg';
-import './App.css';
-// import './Components/Home';
-// import './Components/Recipes';
-// import './Components/Blog';
-// import './Components/Delis';
-// import './Components/Shop';
-// import './Components/Contact';
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
+import Home from './Components/Home';
 
-const Home = () => {
-  <div><h2>Home</h2></div>
-}
+const About = () => (
+  <div>
+    <h2>About</h2>
+  </div>
+)
 
-const Recipes = () => {
-  <div><h2>Recipes</h2></div>
-}
+const Blog = () => (
+  <div>
+    <h2>Blog</h2>
+  </div>
+)
 
-const Blog = () => {
-  <div><h2>Blog</h2></div>
-}
+const Contact = () => (
+  <div>
+    <h2>Contact</h2>
+  </div>
+)
 
-const Delis = () => {
-  <div><h2>Delis</h2></div>
-}
+const Shop = () => (
+  <div>
+    <h2>Shop</h2>
+  </div>
+)
 
-const Shop = () => {
-  <div><h2>Shop</h2></div>
-}
+const Topic = ({ match }) => (
+  <div>
+    <h3>{match.params.topicId}</h3>
+  </div>
+)
 
-const Contact = () => {
-  <div><h2>Contact</h2></div>
-}
+const Deli = ({ match }) => (
+  <div>
+    <h3>{match.params.deliId}</h3>
+  </div>
+)
 
-const App = () => {
+const Recipe = ({ match }) => (
+  <div>
+    <h3>{match.params.recipeId}</h3>
+  </div>
+)
+
+const Topics = ({ match }) => (
+  <div>
+    <h2>Topics</h2>
+    <ul>
+      <li>
+        <Link to={`${match.url}/rendering`}>
+          Rendering with React
+        </Link>
+      </li>
+      <li>
+        <Link to={`${match.url}/components`}>
+          Components
+        </Link>
+      </li>
+      <li>
+        <Link to={`${match.url}/props-v-state`}>
+          Props v. State
+        </Link>
+      </li>
+    </ul>
+
+    <Route path={`${match.url}/:topicId`} component={Topic}/>
+    <Route exact path={match.url} render={() => (
+      <h3>Please select a topic.</h3>
+    )}/>
+  </div>
+)
+
+const Delis = ({ match }) => (
+  <div>
+    <h2>Delis</h2>
+    <ul>
+      <li>
+        <Link to={`${match.url}/kitchenCounter`}>
+          The Kitchen Counter
+        </Link>
+      </li>
+      <li>
+        <Link to={`${match.url}/weighhouseSt`}>
+          Weighhouse Street
+        </Link>
+      </li>
+      <li>
+        <Link to={`${match.url}/seymourPlace`}>
+          Seymour Place
+        </Link>
+      </li>
+    </ul>
+
+    <Route path={`${match.url}/:deliId`} component={Deli}/>
+    <Route exact path={match.url} render={() => (
+      <h3>Please select a Deli.</h3>
+    )}/>
+  </div>
+)
+
+const Recipes = ({ match }) => (
+  <div>
+    <h2>Recipes</h2>
+    <ul>
+      <li>
+        <Link to={`${match.url}/kitchenCounter`}>
+          The Kitchen Counter
+        </Link>
+      </li>
+      <li>
+        <Link to={`${match.url}/weighhouseSt`}>
+          Weighhouse Street
+        </Link>
+      </li>
+      <li>
+        <Link to={`${match.url}/seymourPlace`}>
+          Seymour Place
+        </Link>
+      </li>
+    </ul>
+
+    <Route path={`${match.url}/:deliId`} component={Deli}/>
+    <Route exact path={match.url} render={() => (
+      <h3>Please select a Recipe.</h3>
+    )}/>
+  </div>
+)
+
+
+const BasicExample = () => (
   <Router>
     <div>
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/recipes">Recipes</Link></li>
-        <li><Link to="/blog">Blog</Link></li>
-        <li><Link to="/delis">Delis</Link></li>
-        <li><Link to="/shop">Shop</Link></li>
-        <li><Link to="/contact">Contact</Link></li>
-      </ul>
+      <div className="main-nav-wrapper">
+        <ul className="main-nav-list">
+          <li className="user-nav-list-item clickable"><Link to="/about">About</Link></li>
+          <li className="user-nav-list-item clickable"><Link to="/recipes">Recipes</Link></li>
+          <li className="user-nav-list-item clickable"><Link to="/blog">Blog</Link></li>
+          <li className="user-nav-list-item clickable list-item-logo"><Link to="/">Deliciously Ella</Link></li>
+          <li className="user-nav-list-item clickable"><Link to="/delis">Delis</Link></li>
+          <li className="user-nav-list-item clickable"><Link to="/shop">Shop</Link></li>
+          <li className="user-nav-list-item clickable"><Link to="/contact">Contact</Link></li>
+        </ul>
+      </div>
+
       <hr/>
 
-      <Route exact path="/" component={Home} />
+      <Route exact path="/" component={Home}/>
+      <Route path="/about" component={About}/>
+      <Route path="/topics" component={Topics}/>
+
       <Route path="/recipes" component={Recipes} />
       <Route path="/blog" component={Blog} />
       <Route path="/delis" component={Delis} />
@@ -54,7 +161,6 @@ const App = () => {
       <Route path="/contact" component={Contact} />
     </div>
   </Router>
+)
 
-}
-
-export default App
+export default BasicExample
